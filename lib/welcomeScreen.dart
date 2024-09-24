1927209d1c6f2c88ee1c7caa2ea254ca6f1895dc
exam <exam@Aptechnk1.edu>
2024-09-24 20:54:27 +0500

    shariq

---
+++ lib/welcomeScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        height: 200,
+        width: 200,
+        child: Text(
+          "shariq",
+          style: TextStyle(color: Colors.white ),
+        ),
+      ),
+    );
+  }
+}
